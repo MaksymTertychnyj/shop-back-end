f9429b688240eb8b871a0a50a190d425f0084864
MaksymTertychnyj <maka12008@ukr.net>
2022-05-12 10:54:07 +0300

    added LoginController, configured WebApi, added Jwt section to appsettings.json

---
+++ Shop.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using Shop.Data.Entities;
+using Shop.Domain.Dto;
+using Shop.Domain.Helpers;
+using Shop.Domain.Services.Interfaces;
+
+
+namespace Shop.WebApi.Controllers
+{
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+               this.loginService = loginService;
+        }
+
+        [Authorize]
+        [HttpGet("/getUser")]
+        public User GetUser()
+        {
+            return HttpContext.Items["User"] as User;
+        }
+
+        [Authorize]
+        [HttpPost("/register")]
+        public async Task<IActionResult> Register([FromBody] User user)
+        {
+            var response = await loginService.Register(user);
+
+            if (response == null)
+            {
+                return BadRequest(new { message = "didn't register!" });
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("/authenticate")]
+        public IActionResult Authenticate([FromBody] UserAuthenticateRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+
+            var response = loginService.Authenticate(request);
+
+            if (response == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+
+            return Ok(response);
+        }
+    }
+}
--- Shop.WebApi/Program.cs
+++ Shop.WebApi/Program.cs
@@ -1,9 +1,33 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Shop.Data.Context;
+using Shop.Data.Entities;
+using Shop.Data.Infrastructure;
+using Shop.Domain.Helpers;
+using Shop.Domain.Services.Implementation;
+using Shop.Domain.Services.Interfaces;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthorization();
+
+builder.Services.AddCors();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ShopContext>(options => 
+            options.UseSqlServer(builder.Configuration
+            .GetConnectionString("DefaultConnection"))
+            );
+
+builder.Services.AddScoped<IRepository<User>, Repository<User>>();
+builder.Services.AddScoped<ILoginService, LoginService>();
 
 var app = builder.Build();
 
@@ -13,10 +37,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.UseMiddleware<JwtMiddleware>();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.Run();
