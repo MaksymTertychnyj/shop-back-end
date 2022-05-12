16ab2b12c28c2e605d94aaad9117bf457be134c5
MaksymTertychnyj <maka12008@ukr.net>
2022-05-12 10:46:30 +0300

    added Repository

---
+++ Shop.Data/Infrastructure/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Infrastructure
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+        IQueryable<TEntity> Query(params Expression<Func<TEntity, object>>[] includes);
+
+        ValueTask<TEntity?> GetByIdAsync(object key);
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task<int> SaveChangesAsync();
+
+    }
+}
---
+++ Shop.Data/Infrastructure/Repository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Infrastructure
+{
+    public sealed class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly ShopContext context;
+        private readonly DbSet<TEntity> dbEntities;
+
+        public Repository(ShopContext context)
+        {
+            this.context = context;
+            dbEntities = this.context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            return (await dbEntities.AddAsync(entity)).Entity;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public ValueTask<TEntity?> GetByIdAsync(object key)
+        {
+            return dbEntities.FindAsync(key);
+        }
+
+        public IQueryable<TEntity> Query(params Expression<Func<TEntity, object>>[] includes)
+        {
+            var dbSet = context.Set<TEntity>();
+            var query = includes
+                .Aggregate<Expression<Func<TEntity, object>>, IQueryable<TEntity>>(dbSet, (current, include) => current.Include(include));
+
+            return query ?? dbSet;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            return await Task.Run(() => dbEntities.Update(entity).Entity);
+        }
+    }
+}
