1a80c36829d003b2fb828751ca4705d92e4f4b81
MaksymTertychnyj <maka12008@ukr.net>
2022-05-12 10:51:22 +0300

    added LoginService

---
+++ Shop.Domain/Services/Implementation/LoginService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Shop.Data.Entities;
+using Shop.Data.Infrastructure;
+using Shop.Domain.Dto;
+using Shop.Domain.Services.Interfaces;
+using Shop.Domain.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Services.Implementation
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IRepository<User> repository;
+        private readonly IConfiguration configuration;
+
+        public LoginService(IRepository<User> repository, IConfiguration configuration)
+        {
+            this.repository = repository;
+            this.configuration = configuration;
+        }
+
+        public UserAuthenticateResponse Authenticate(UserAuthenticateRequest request)
+        {
+            var user = repository.GetByIdAsync(request.Login).Result;
+
+            if (user != null && user.Password == request.Password)
+            {
+                var token = configuration.GenerateJwtToken(user);
+
+                return new UserAuthenticateResponse(user, token);
+            }
+
+            return null!;
+        }
+
+
+        public async Task<UserAuthenticateResponse> Register(User user)
+        {
+            var addedUser = await repository.AddAsync(user);
+
+            await repository.SaveChangesAsync();
+
+            var response = Authenticate(new UserAuthenticateRequest
+            {
+                Login = user.Login,
+                Password = user.Password
+            });
+
+            return response;
+        }
+    }
+}
---
+++ Shop.Domain/Services/Interfaces/ILoginService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Shop.Data.Entities;
+using Shop.Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Services.Interfaces
+{
+    public interface ILoginService
+    {
+        UserAuthenticateResponse Authenticate(UserAuthenticateRequest request);
+
+        Task<UserAuthenticateResponse> Register(User user);
+    }
+}
