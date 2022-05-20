320c9c52ae09ea3b846b702fc543722efa3589a0
MaksymTertychnyj <maka12008@ukr.net>
2022-05-20 15:25:34 +0300

    installed packages, added CategoryDTO

---
+++ Shop.Domain/Dto/CategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Dto
+{
+    public class CategoryDTO
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
