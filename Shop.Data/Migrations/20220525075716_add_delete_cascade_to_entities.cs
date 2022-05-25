8ee9854e359938b5a1cbfb1a1964e1912f05d8e9
MaksymTertychnyj <maka12008@ukr.net>
2022-05-25 10:59:56 +0300

    add migration, update database

---
+++ Shop.Data/Migrations/20220525075716_add_delete_cascade_to_entities.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Data.Migrations
+{
+    public partial class add_delete_cascade_to_entities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
