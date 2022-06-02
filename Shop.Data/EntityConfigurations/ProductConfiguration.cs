692279a92ac96155d7409fa0ddc77f1570cf5494
MaksymTertychnyj <maka12008@ukr.net>
2022-06-02 09:23:56 +0300

    add migration "renamed Product property Name to JsonParameters"

--- Shop.Data/EntityConfigurations/ProductConfiguration.cs
+++ Shop.Data/EntityConfigurations/ProductConfiguration.cs
@@ -16,12 +16,11 @@ namespace Shop.Data.EntityConfigurations
             builder.ToTable("Products");
             builder.HasKey(product => product.Id);
             builder.HasIndex(product => product.Id).IsUnique(true);
-            builder.HasIndex(p => p.Name).IsUnique(true);
             builder.HasOne(product => product.Category)
                 .WithMany(category => category.Products)
                 .HasForeignKey(product => product.CategoryId)
                 .OnDelete(DeleteBehavior.Cascade);
-            builder.Property(p => p.Name).IsRequired(true);
+            builder.Property(p => p.JsonParameters).IsRequired(true);
             builder.Property(p => p.CategoryId).IsRequired(true);
             builder.Property(p => p.Id).UseIdentityColumn();
         }
---
+++ Shop.Data/Migrations/20220602062201_RenamedProductPropertyName.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Data.Migrations
+{
+    public partial class RenamedProductPropertyName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Name",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JsonParameters",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JsonParameters",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Name",
+                table: "Products",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
