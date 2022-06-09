03f3c3d1475fe4453bb6f201f8ad87f30ad656d4
MaksymTertychnyj <maka12008@ukr.net>
2022-06-09 17:12:20 +0300

    add models: Order, OrderAddress, OrderProduct

---
+++ Shop.Data/Entities/Orders/Order.cs
@@ -0,0 +1,21 @@
+﻿using Shop.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Entities.Orders
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime DateRegister { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.negotiation;
+        public int OrderAddressId { get; set; }
+        public OrderAddress? OrderAddress { get; set; }
+        public string CustomerLogin { get; set; } = string.Empty;
+        public Customer? Customer { get; set; }
+        public ICollection<OrderProduct> Products { get; set; } = new List<OrderProduct>();
+    }
+}
---
+++ Shop.Data/Entities/Orders/OrderAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Entities.Orders
+{
+    public class OrderAddress
+    {
+        public int Id { get; set; }
+        public string Country { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public int OrderId { get; set; }
+        public Order? Order { get; set; }
+    }
+}
