c8112740e38c0cd9bdd6525b31d554d30e0d3b2b
MaksymTertychnyj <maka12008@ukr.net>
2022-06-10 18:58:21 +0300

    resolved bag with cycle of load navigation properties

--- Shop.WebApi/Program.cs
+++ Shop.WebApi/Program.cs
@@ -16,7 +16,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options => 
+        options.SerializerSettings
+        .ReferenceLoopHandling = Newtonsoft
+        .Json.ReferenceLoopHandling.Ignore
+     );
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAuthorization();
 builder.Services.AddCors();
