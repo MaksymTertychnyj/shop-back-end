058d13a018cd035b0cca258de46a98a89d8029e1
MaksymTertychnyj <maka12008@ukr.net>
2022-06-17 13:57:05 +0300

    added helper, mapping

---
+++ NovaPochta/Helpers/HttpClientHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace NovaPochta.Helpers
+{
+    public static class HttpClientHelper
+    {
+        public static string ApiKey { get; set; } = string.Empty;
+        public static string GetApiKey(this HttpClient client)
+        {
+            return ApiKey;
+        }
+
+        public static void SetApiKey(this HttpClient client, string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+    }
+}
---
+++ NovaPochta/Mapping/MapRequestProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NovaPochta.Dto;
+
+namespace NovaPochta.Mapping
+{
+    public class MapRequestProfile : Profile
+    {
+        public MapRequestProfile()
+        {
+            CreateMap<Request, RequestDto>()
+                .ReverseMap();
+        }
+    }
+}
