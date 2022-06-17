6475ae04d6e5915a0fcf233b08cda3ab927c7ab2
MaksymTertychnyj <maka12008@ukr.net>
2022-06-17 11:43:49 +0300

    add and register MapRequestProfile

---
+++ Shop.Domain/Mapping/NovaPochta/MapRequestProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Shop.Domain.Dto.NovaPochta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Mapping.NovaPochta
+{
+    public class MapRequestProfile : Profile
+    {
+        public MapRequestProfile()
+        {
+            CreateMap<Request, RequestDto>()
+                .ReverseMap();
+        }
+    }
+}
