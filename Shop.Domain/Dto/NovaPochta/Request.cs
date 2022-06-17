983acef1ed56d0169795fee544a51ac6098c1002
MaksymTertychnyj <maka12008@ukr.net>
2022-06-17 11:42:56 +0300

    add Request model

---
+++ Shop.Domain/Dto/NovaPochta/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Dto.NovaPochta
+{
+    public class Request
+    {
+        public string apiKey { get; set; } = string.Empty;
+        public string modelName { get; set; } = string.Empty;
+        public string calledMethod { get; set; } = string.Empty;
+        public object? methodProperties { get; set; }
+    }
+}
