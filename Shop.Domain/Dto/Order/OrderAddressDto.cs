ea2f0b3e8ddc5f5da0a70ddc796ba284c11d3805
MaksymTertychnyj <maka12008@ukr.net>
2022-06-21 11:51:58 +0300

    added OrderAddressDto

---
+++ Shop.Domain/Dto/Order/OrderAddressDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Dto.Order
+{
+    public class OrderAddressDto
+    {
+        public string Country { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
