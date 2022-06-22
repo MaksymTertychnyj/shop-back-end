b8e038f79013530593f303b01734e7f71947ca52
MaksymTertychnyj <maka12008@ukr.net>
2022-06-22 11:16:36 +0300

    changed Dto's

--- Shop.Domain/Dto/Order/OrderProductDto.cs
+++ Shop.Domain/Dto/Order/OrderProductDto.cs
@@ -3,7 +3,7 @@
     public class OrderProductDto
     {
         public string Name { get; set; } = string.Empty;
-        public int Price { get; set; }
+        public double Price { get; set; }
         public int Quantity { get; set; }
         public string JsonParameters { get; set; } = string.Empty;
     }
