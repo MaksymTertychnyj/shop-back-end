3d9cacd93676a4af8ffdd60b9cb2d174b0795b60
MaksymTertychnyj <maka12008@ukr.net>
2022-06-27 19:19:37 +0300

    added update methods to OrderController

--- Shop.WebApi/Controllers/OrderController.cs
+++ Shop.WebApi/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Data.Entities.Orders;
+using Shop.Data.Enums;
 using Shop.Domain.Dto.Order;
 using Shop.Domain.Helpers;
 using Shop.Domain.Services.Interfaces;
@@ -30,7 +31,6 @@ namespace Shop.WebApi.Controllers
         public async Task<IActionResult> GetOrderById([FromRoute]int orderId)
         {
             var order = await orderService.GetOrderByIdAsync(orderId);
-
             return order != null ? Ok(order) : NotFound();
         }
 
@@ -39,39 +39,40 @@ namespace Shop.WebApi.Controllers
         public async Task<IActionResult> GetOrdersByCustomer([FromRoute]string customerLogin)
         {
             var orders = await orderService.GetOrdersByCustomerAsync(customerLogin);
-
             return orders != null ? Ok(orders) : NotFound();
         }
 
         [Authorize]
         [HttpPost("add")]
-        public async Task<IActionResult> ConfirmOrder([FromBody] OrderDto order)
+        public async Task<IActionResult> ConfirmOrder([FromBody]OrderDto order)
         {
             var result = await orderService.ConfirmOrderAsync(order);
-
             return result != null ? Ok(result) : BadRequest(new { message = "confirming the order has been failed" });
         }
 
-        [Authorize]
+        [Authorize(Roles = "admin, user")]
         [HttpDelete("delete/{orderId}")]
         public async Task<IActionResult> DeleteOrder([FromRoute]int orderId)
         {
             var result = await orderService.DeleteOrderAsync(orderId);
-         
             return result ? Ok() : BadRequest(new { message = "deleting the order has been failed" });
         }
 
-        //[Authorize]
-        //[HttpPut("update")]
-        //public async Task<IActionResult> UpdateOrderAsync([FromBody] Order order)
-        //{
-        //    var result = await orderService.UpdateEntityAsync(order, order.Id);
-        //    if (result != null)
-        //    {
-        //        return Ok(result);
-        //    }
+        [Authorize(Roles = "admin, user")]
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateOrder([FromBody]OrderDto order)
+        {
+            var result = await orderService.UpdateOrderAsync(order);
+            return result != null ? Ok(result) : BadRequest(new { message = "updating the order has been failed" });
+        }
 
-        //    return BadRequest(new { message = "updating the order has been failed" });
-        //}
+        [Authorize(Roles = "admin, user")]
+        [HttpPut("updateStatus/{status}")]
+        public async Task<IActionResult> UpdateOrderStatus([FromBody]OrderDto order, [FromRoute]OrderStatus status)
+
+        {
+            var result = await orderService.UpdateStatusAsync(order, status);
+            return result != null ? Ok(result) : BadRequest(new { message = "updating the status has been failed" });
+        }
     }
 }
